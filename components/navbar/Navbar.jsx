b173e7a859ef56322045a890b2d4a45112dd5fb4
Umang Sailor <ursailor@gmail.com>
2023-10-26 17:44:52 +0530

    :)

---
+++ components/navbar/Navbar.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import styles from "./navbar.module.css";
+import Image from "next/image";
+import Link from "next/link";
+// import AuthLinks from "../authLinks/AuthLinks";
+// import ThemeToggle from "../themeToggle/ThemeToggle";
+
+const Navbar = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.logo}>Spirited Score</div>
+      <div className={styles.links}>
+        {/* <ThemeToggle /> */}
+        <Link href="/" className={styles.link}>Homepage</Link>
+        <Link href="/" className={styles.link}>Contact</Link>
+        <Link href="/" className={styles.link}>About</Link>
+        {/* <AuthLinks /> */}
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
