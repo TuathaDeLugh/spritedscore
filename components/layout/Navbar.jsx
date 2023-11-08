93d2ccc90a720e234a73dd0edb028ffaba8444c3
Umang Sailor <61745264+TuathaDeLugh@users.noreply.github.com>
2023-11-08 23:16:26 +0530

    Update Navbar.jsx

--- components/layout/Navbar.jsx
+++ components/layout/Navbar.jsx
@@ -46,7 +46,7 @@ export default function Navbar() {
         
       <div>
         <div
-          className={`flex-1 justify-self-center pb-3 mt-8 md:block md:pb-0 md:mt-0 ${navbar ? 'p-12 md:p-0 block' : 'hidden'
+          className={`flex-1 justify-self-center pb-3 mt-4 md:block md:pb-0 md:mt-0 ${navbar ? 'p-12 md:p-0 block' : 'hidden'
         }`}
         >
           <ul className="h-screen md:h-auto items-center justify-center md:flex ">
