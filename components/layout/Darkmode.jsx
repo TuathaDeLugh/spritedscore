e93638a7ad618cf061f42a8ff9c316783538c075
Umang Sailor <ursailor@gmail.com>
2023-11-08 22:30:00 +0530

    remove time from old dark mode

--- components/layout/Darkmode.jsx
+++ components/layout/Darkmode.jsx
@@ -1,44 +1,46 @@
 "use client"
 import { useState, useEffect } from 'react';
+import Cookies from 'js-cookie';
 import { FiSun,FiMoon } from "react-icons/fi";
 export default function Darkmode ({ children}) {
     const [isDarkMode, setIsDarkMode] = useState(false);
 
-const toggleDarkMode = () => {
-  const newDarkModeStatus = !isDarkMode;
-  setIsDarkMode(newDarkModeStatus);
-  localStorage.setItem('dark-mode', JSON.stringify(newDarkModeStatus)); // Save to localStorage
-};
+  const toggleDarkMode = () => {
+    const newDarkModeStatus = !isDarkMode;
+    setIsDarkMode(newDarkModeStatus);
+    Cookies.set('dark-mode', newDarkModeStatus);
+  };
 
-useEffect(() => {
-  const localStorageDarkMode = localStorage.getItem('dark-mode');
+  useEffect(() => {
+    // Check if dark mode preference is set in the cookie
+    const cookieDarkMode = Cookies.get('dark-mode');
+    
+    // If cookieDarkMode is not set, check user's system preference
+    if (cookieDarkMode === undefined) {
+      const systemDarkMode =
+        window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
+      setIsDarkMode(systemDarkMode);
+    } else {
+      setIsDarkMode(cookieDarkMode === 'true');
+    }
+  }, []);
 
-  if (localStorageDarkMode === null) {
-    const systemDarkMode =
-      window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
-    setIsDarkMode(systemDarkMode);
-  } else {
-    setIsDarkMode(JSON.parse(localStorageDarkMode));
-  }
-}, []);
+  useEffect(() => {
+    if (isDarkMode) {
+      document.documentElement.classList.add('dark');
+    } else {
+      document.documentElement.classList.remove('dark');
+    }
+  }, [isDarkMode]);
 
-useEffect(() => {
-  if (isDarkMode) {
-    document.documentElement.classList.add('dark');
-  } else {
-    document.documentElement.classList.remove('dark');
-  }
-}, [isDarkMode]);
-
-return (
-  
-  <div className={isDarkMode ? 'dark' : ''}>
-    {children}
-    <div className='z-50 text-white fixed bottom-10 right-0 px-1 border border-slate-300 dark:border-slate-700 shadow-sm shadow-slate-600 backdrop-blur p-1 rounded-l-full'>
-      <button onClick={toggleDarkMode} className='rounded-full p-2 bg-purple-600'>
-        {isDarkMode ? <FiSun size={25} /> : <FiMoon size={25} />}
-      </button>
+  return (
+    <div className={isDarkMode ? 'dark' : ''}>
+      {children}
+        <div className='z-50 text-white fixed bottom-10 right-0 px-1 border border-slate-300 dark:border-slate-700 shadow-sm shadow-slate-600 backdrop-blur p-1 rounded-l-full'>
+      <button onClick={toggleDarkMode} className=' rounded-full p-2 bg-purple-600'>
+          {isDarkMode ? <FiSun size={25}/> : <FiMoon size={25}/>}
+          </button>
+          </div>
     </div>
-  </div>
-);
-}
+  );
+};
