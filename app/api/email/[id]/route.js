ca0f47c1354e7ee0d8b0b5099e3b7f2ff9bfcc31
Umang Sailor <ursailor@gmail.com>
2023-11-09 15:56:30 +0530

    error fix

--- app/api/email/[id]/route.js
+++ app/api/email/[id]/route.js
@@ -1,5 +1,5 @@
-import connectdb from "@/database/connection";
 import Email from "@/models/mail";
+import connectdb from "@/util/mongodb";
 import { NextResponse } from "next/server";
 
 export async function GET(request, { params }) {
