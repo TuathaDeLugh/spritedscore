7fb8ce94dfb2578a614403cb952a6eae3fd195c4
Umang Sailor <ursailor@gmail.com>
2023-11-16 12:20:10 +0530

    fix

--- controller/allreview.js
+++ controller/allreview.js
@@ -1,18 +1,17 @@
-export default async function getAllReviews() {
-    try {
-      const api = process.env.API_URL;
-      console.log(api);
-      // const response = await fetch(`${api}/api/review`, 
-      // const response = await fetch(`https://spiritrdscore.vercel.app/api/review`, 
+export default async function getAllReview() {
+  try {
+    const api = process.env.API_URL;
+    const response = await fetch(`${api}/api/review/`, 
 
-      const response = await fetch(`http://localhost:3000/api/review`, 
+    // const response = await fetch(`http://localhost:3000/api/review/user/${id}`, 
 
-      {
-        cache: "no-store",
-      });
-      const reviews = await response.json();
-      return reviews.data;
-    } catch (error) {
-      console.log(error);
-    }
+    
+    {
+      cache: "no-store",
+    });
+    const reviews = await response.json();
+    return reviews.data;
+  } catch (error) {
+    console.log(error);
   }
+}
