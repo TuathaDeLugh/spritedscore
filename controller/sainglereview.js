d6b129ee65325e65ab5a3c9f505b24514d3b8b57
Umang Sailor <ursailor@gmail.com>
2023-11-17 15:22:52 +0530

    dumb mistake fix

--- controller/sainglereview.js
+++ controller/sainglereview.js
@@ -2,9 +2,9 @@ export default async function getSingleReview(id) {
     try {
       const api = process.env.API_URL;
 
-    //   const response = await fetch(`${api}/api/review/${id}`, 
+      const response = await fetch(`${api}/api/review/${id}`, 
 
-      const response = await fetch(`http://localhost:3000/api/review/${id}`, 
+      // const response = await fetch(`http://localhost:3000/api/review/${id}`, 
 
       
       {
