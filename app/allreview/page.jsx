a331e87b5e321692af50bc8d0984360288606b73
Umang Sailor <ursailor@gmail.com>
2023-11-17 15:16:34 +0530

    😐

--- app/allreview/page.jsx
+++ app/allreview/page.jsx
@@ -1,40 +1,36 @@
-// import FatchAllreview from '@/components/pages/FatchAllreview';
-// import React, { Suspense} from 'react';
-// import ReviewDropDown from '@/components/ReviewDropDown';
-// import SkelReview from '@/components/layout/SkelReview';
+import FatchAllreview from '@/components/pages/FatchAllreview';
+import React, { Suspense} from 'react';
+import ReviewDropDown from '@/components/ReviewDropDown';
+import SkelReview from '@/components/layout/SkelReview';
 
-// export default function AllReviews() {
+export default function AllReviews() {
 
     
-//     return (
-//         <section className="px-2 mx-auto max-w-[1500px] md:pt-20 pt-16">
-//             <div className="container px-6 py-5 mx-auto">
-//             <span className="mb-4 block text-base font-semibold  text-purple-700 dark:text-purple-400">
-//             All reviews
-//         </span>
-//         <div className="flex items-center justify-between">
+    return (
+        <section className="px-2 mx-auto max-w-[1500px] md:pt-20 pt-16">
+            <div className="container px-6 py-5 mx-auto">
+            <span className="mb-4 block text-base font-semibold  text-purple-700 dark:text-purple-400">
+            All reviews
+        </span>
+        {/* <div className="flex items-center justify-between">
 
-//         <h2 className="mb-6 text-[32px] font-bold capitalize text-dark lg:text-[4xl] ">
-//         Unlock The Essence Of Every Anime With Spirited Score&apos;s Insightful Reviews
-//         </h2>
+        <h2 className="mb-6 text-[32px] font-bold capitalize text-dark lg:text-[4xl] ">
+        Unlock The Essence Of Every Anime With Spirited Score&apos;s Insightful Reviews
+        </h2>
 
-//                     <ReviewDropDown/>
+                    <ReviewDropDown/>
 
         
         
         
         
-//             </div>
+            </div>
             
 
-//             <Suspense fallback={<SkelReview/>}>
-//                    <FatchAllreview/>
-//             </Suspense>
-//             </div>
-//         </section>
-//     );
-// }
-
-
-
-
+            <Suspense fallback={<SkelReview/>}>
+                   <FatchAllreview/>
+            </Suspense> */}
+            </div>
+        </section>
+    );
+}
