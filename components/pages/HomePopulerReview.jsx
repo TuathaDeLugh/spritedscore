3b18dbb1a4f1ac49aaed7fbab32f192714663717
Umang Sailor <ursailor@gmail.com>
2023-11-20 10:07:02 +0530

    box size fix

--- components/pages/HomePopulerReview.jsx
+++ components/pages/HomePopulerReview.jsx
@@ -9,12 +9,12 @@ export default async function HomePopulerReview() {
 			<span className='mb-4 block text-base font-semibold  text-purple-700 dark:text-purple-400'>
 				Popular reviews by category
 			</span>
-			<div className="overflow-auto pr-5 flex flex-wrap justify-between">
+			<div className="overflow-auto pr-5 flex flex-wrap justify-between gap-3">
 			{reviews?.map(review => {
 				return (
 
 			<Link href={`/allreview/${review._id}`} key={review._id}
-  class="inline-block w-[45%] mb-5 rounded-lg bg-slate-50 shadow hover:border-purple-500 dark:hover:border-purple-400 dark:shadow-gray-600 dark:bg-slate-800 border dark:border-gray-600">
+  class="inline-block grow w-40 mb-5 rounded-lg bg-slate-50 shadow hover:border-purple-500 dark:hover:border-purple-400 dark:shadow-gray-600 dark:bg-slate-800 border dark:border-gray-600">
 	<div className='w-full'>
   <div
     class="border-b border-gray-200 px-6 py-3 dark:border-gray-600 dark:text-neutral-50">
@@ -26,7 +26,7 @@ export default async function HomePopulerReview() {
       {review.title}
     </h5>
     <p class="mb-4 text-base text-neutral-600 dark:text-neutral-200">
-      {review.rating}/10
+    &#9733;{review.rating}/10
     </p>
   </div>
   </div>
