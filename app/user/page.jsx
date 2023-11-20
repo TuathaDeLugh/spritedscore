9560835bfd7a6772c9d635bdc3485523f6612976
Umang Sailor <ursailor@gmail.com>
2023-11-20 11:24:20 +0530

    card design fix

---
+++ app/user/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Profile() {
+  return (
+    <div className='px-2 mx-auto max-w-[1500px] md:pt-20 pt-16'>
+			<div className='container flex flex-col justify-between px-6 md:px-0 py-4 mx-auto space-y-6 lg:h-[32rem] lg:py-16 lg:flex-row lg:items-center gap-6'>
+               This is profile page
+                </div>
+                </div>
+  )
+}
--- components/pages/HomePopulerReview.jsx
+++ components/pages/HomePopulerReview.jsx
@@ -9,29 +9,31 @@ export default async function HomePopulerReview() {
 			<span className='mb-4 block text-base font-semibold  text-purple-700 dark:text-purple-400'>
 				Popular reviews by category
 			</span>
-			<div className="overflow-auto pr-5 flex flex-wrap justify-between gap-3">
+			<div className="overflow-auto gap-5 pr-5 flex flex-wrap justify-between">
 			{reviews?.map(review => {
 				return (
 
 			<Link href={`/allreview/${review._id}`} key={review._id}
-  class="inline-block grow w-40 mb-5 rounded-lg bg-slate-50 shadow hover:border-purple-500 dark:hover:border-purple-400 dark:shadow-gray-600 dark:bg-slate-800 border dark:border-gray-600">
-	<div className='w-full'>
-  <div
-    class="border-b border-gray-200 px-6 py-3 dark:border-gray-600 dark:text-neutral-50">
-    {review.categoryName}
+  class="inline-block grow w-40 rounded-lg bg-slate-50 shadow hover:border-purple-500 dark:hover:border-purple-400 dark:shadow-gray-600 dark:bg-slate-800 border dark:border-gray-600">
+ 
+<div className="relative rounded-t-lg">
+  <img
+    className="w-full rounded-t-lg h-32 object-cover object-center"
+    src={review.image.link}
+    alt={`Category: ${review.image.name}`}
+  />
+  <div className="absolute inset-0 rounded-t-lg bg-black opacity-50"></div>
+  <div className="absolute inset-0 text-white flex items-center justify-center">
+    <h2 className="text-xl font-bold">{review.categoryName}</h2>
   </div>
-  <div class="p-6">
-    <h5
-      class="mb-2 text-xl font-medium leading-tight text-purple-500 dark:text-purple-400">
-      {review.title}
-    </h5>
-    <p class="mb-4 text-base text-neutral-600 dark:text-neutral-200">
-    &#9733;{review.rating}/10
-    </p>
+</div>
+<div className="px-4 py-2 bg-slate-50 dark:bg-slate-800 rounded-b-lg">
+  <h3 className="text-lg font-semibold">{review.title}</h3>
+  <div className="flex items-center">
+    <span className="text-sm text-gray-600 dark:text-gray-300">Rating: {review.rating}/10</span>
   </div>
-  </div>
+</div>
 </Link>
-
 )}
 )}
 </div>
