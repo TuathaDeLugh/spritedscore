8bb70bb1e204a450623278af7d2a875c897a846c
Umang Sailor <ursailor@gmail.com>
2023-11-24 19:02:02 +0530

    ' fix

--- components/pages/EditProfile.jsx
+++ components/pages/EditProfile.jsx
@@ -147,7 +147,7 @@ export default function EditProfile({ userdata }) {
           onBlur={handleBlur}
         />
       </ div>
-      {userdata.provider === "google" ? (<p className=" text-red-600 dark:text-red-500 text-sm">You can't update email because you logged in using Google</p>) : null}
+      {userdata.provider === "google" ? (<p className=" text-red-600 dark:text-red-500 text-sm">You can&apos;t update email because you logged in using Google</p>) : null}
 
         {errors.email && touched.email ? (
           <p className=" text-red-600 dark:text-red-500 text-sm">* {errors.email}</p>
