8292448cf79e905c0b4b97a172ccc377e1e015aa
Umang Sailor <ursailor@gmail.com>
2023-11-24 18:49:36 +0530

    user update username corelated review also update

--- app/api/user/[id]/route.js
+++ app/api/user/[id]/route.js
@@ -1,3 +1,4 @@
+import Review from "@/models/review";
 import User from "@/models/user";
 import connectdb from "@/util/mongodb";
 import { NextResponse } from "next/server";
@@ -9,10 +10,19 @@ export async function GET(request, { params }) {
     return NextResponse.json({ data:user }, { status: 200 });
 
 }
-export async function PUT(request,{params}){
+export async function PUT(request, { params }) {
     const { id } = params;
-    const {name,username,email}  = await request.json();
-    await connectdb();
-    await User.findByIdAndUpdate(id, {name,username,email} );
-    return NextResponse.json({message:"Profile Updated"},{status:200});
-}
+    const { name, username, email } = await request.json();
+      await connectdb();
+      const updatedUser = await User.findByIdAndUpdate(id, { name, username, email }, { new: true });
+      await Review.updateMany(
+        { 'creator.userid': id },
+        {
+          $set: {
+            'creator.createdby': updatedUser.username,
+            'creator.avatar': updatedUser.avatar,
+          },
+        }
+      );
+      return NextResponse.json({ message: 'Profile and associated reviews updated' }, { status: 200 });
+  }
