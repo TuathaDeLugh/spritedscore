47aeb5653c238229b21b82d236af51e37c5c0c31
dharmik48 <hingudharmik4@gmail.com>
2023-11-27 15:56:52 +0530

    feat: adds watchlist

--- models/user.js
+++ models/user.js
@@ -1,18 +1,22 @@
 import { Schema, model, models } from 'mongoose'
 
 const userSchema = new Schema(
-    {  
-        username: { type: String, required: true, unique: true },
-        name: String,
-        avatar: String,
-        email: { type: String, required: true, unique: true },
-        password: String,
-        provider:String,
-        role:String
-
-},
-{ 
-    timestamps: true
- }
+	{
+		username: { type: String, required: true, unique: true },
+		name: String,
+		avatar: String,
+		email: { type: String, required: true, unique: true },
+		password: String,
+		provider: String,
+		role: String,
+		watchlist: {
+			type: [Schema.Types.ObjectId],
+			ref: 'review',
+			unique: true,
+		},
+	},
+	{
+		timestamps: true,
+	}
 )
 export default models.User || model('User', userSchema)
