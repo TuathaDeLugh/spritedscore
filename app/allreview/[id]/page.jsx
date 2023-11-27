d68a8926c5a2f989bd1ead80011dd5df74ef1bfe
dharmik48 <hingudharmik4@gmail.com>
2023-11-27 16:36:39 +0530

    feat: adds watchlist page

--- app/allreview/[id]/page.jsx
+++ app/allreview/[id]/page.jsx
@@ -7,139 +7,186 @@ import { getServerSession } from 'next-auth'
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
-import { AiOutlineUser } from "react-icons/ai";
+import { AiOutlineUser } from 'react-icons/ai'
 
 async function page({ params: { id } }) {
   const session = await getServerSession(authOptions)
   const review = await getSingleReview(id)
   // console.log(review)
   return (
-    <section className="px-2 mx-auto max-w-[1500px] md:pt-20 pt-16">
-      <div className="container px-6 py-5 mx-auto">
+    <section className='px-2 mx-auto max-w-[1500px] md:pt-20 pt-16'>
+      <div className='container px-6 py-5 mx-auto'>
         <div className='mb-4 flex items-center'>
-        <Goback/>
-        <span className=" block text-base font-semibold  text-purple-700 dark:text-purple-400 ">
-          Anime Review
-        </span>
+          <Goback />
+          <span className=' block text-base font-semibold  text-purple-700 dark:text-purple-400 '>
+            Anime Review
+          </span>
         </div>
-        <div className="flex items-center justify-between">
-
-          <h2 className="mb-6 text-[32px] font-bold capitalize text-dark lg:text-[4xl] ">
+        <div className='flex items-center justify-between'>
+          <h2 className='mb-6 text-[32px] font-bold capitalize text-dark lg:text-[4xl] '>
             {review.title}
           </h2>
         </div>
         <div className='flex justify-between flex-wrap lg:border-b dark:border-slate-400 pb-10'>
-
-          <Image alt={review.image.name} className='mx-2 rounded-lg border max-h-[40rem] object-cover w-full lg:w-[70%] xl:w-[77%]' src={review.image.link} width={900} height={600} />
+          <Image
+            alt={review.image.name}
+            className='mx-2 rounded-lg border max-h-[40rem] object-cover w-full lg:w-[70%] xl:w-[77%]'
+            src={review.image.link}
+            width={900}
+            height={600}
+          />
           <div className=' text-lg w-full lg:w-[25%] xl:w-[20%]'>
-            <p className="mt-2  tracking-wider">
-              <span className=' text-purple-500 dark:text-purple-400  font-medium'> Rating : </span>  &#9733; {review.rating}/10
+            <p className='mt-2  tracking-wider'>
+              <span className=' text-purple-500 dark:text-purple-400  font-medium'>
+                {' '}
+                Rating :{' '}
+              </span>{' '}
+              &#9733; {review.rating}/10
             </p>
             <div className='mt-5'>
-              <span className=' text-purple-500 dark:text-purple-400  font-medium'>Category : </span>
-              <div className="flex flex-wrap">
-                {
-                  review.category?.map((category) => {
-                    return (
-                      <span key={category} className='px-2'>{category}</span>
-
-                    );
-                  })
-                }
-
+              <span className=' text-purple-500 dark:text-purple-400  font-medium'>
+                Category :{' '}
+              </span>
+              <div className='flex flex-wrap'>
+                {review.category?.map(category => {
+                  return (
+                    <span key={category} className='px-2'>
+                      {category}
+                    </span>
+                  )
+                })}
               </div>
             </div>
-            <p className="mt-5  tracking-wider">
-              <span className=' text-purple-500 dark:text-purple-400  font-medium'> Episodes : </span> {review.episodes}
+            <p className='mt-5  tracking-wider'>
+              <span className=' text-purple-500 dark:text-purple-400  font-medium'>
+                {' '}
+                Episodes :{' '}
+              </span>{' '}
+              {review.episodes}
             </p>
             <div className='mt-5 w-full'>
-              <span className=' text-purple-500 dark:text-purple-400  font-medium'>Charectors : </span>
-              {
-                (review.characters.length > 0) ? (
-                  <table className='w-full rounded-lg mt-3 '>
-                    <thead>
-
-                      <tr className='text-purple-500 dark:text-purple-400 bg-slate-300 dark:bg-slate-600 border-b dark:border-slate-500'>
-                        <td className='py-1 px-2'>Name</td>
-                        <td className='py-1 px-2'>Status</td>
-                      </tr>
-                    </thead>
-                    <tbody>
-                      {
-                        review.characters?.map((character) => {
-
-                          return (
-                            <tr key={character.name} className='border-b dark:border-slate-500'>
-
-                              <td className='py-1 px-2'>{character.name}</td>
-                              <td className='py-1 px-2'>{character.role}</td>
-
-                            </tr>
-
-                          );
-                        })
-                      }
-                    </tbody>
-                  </table>
-                )
-                  : <p>Charectors not provided by creator</p>
-              }
-
+              <span className=' text-purple-500 dark:text-purple-400  font-medium'>
+                Charectors :{' '}
+              </span>
+              {review.characters.length > 0 ? (
+                <table className='w-full rounded-lg mt-3 '>
+                  <thead>
+                    <tr className='text-purple-500 dark:text-purple-400 bg-slate-300 dark:bg-slate-600 border-b dark:border-slate-500'>
+                      <td className='py-1 px-2'>Name</td>
+                      <td className='py-1 px-2'>Status</td>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {review.characters?.map(character => {
+                      return (
+                        <tr
+                          key={character.name}
+                          className='border-b dark:border-slate-500'
+                        >
+                          <td className='py-1 px-2'>{character.name}</td>
+                          <td className='py-1 px-2'>{character.role}</td>
+                        </tr>
+                      )
+                    })}
+                  </tbody>
+                </table>
+              ) : (
+                <p>Charectors not provided by creator</p>
+              )}
             </div>
-            {
-              (review.trailer == "/") ? null : (
-                <Link href={review.trailer} target='_blank' className="inline-block text-white w-full text-center mt-5 py-2 px-5 rounded-full border text-lg bg-purple-500 dark:bg-purple-400 hover:text-purple-400 hover:border-purple-400 hover:bg-transparent dark:hover:bg-transparent  font-medium">Watch Trailer</Link>
-              )
-            }
+            {review.trailer == '/' ? null : (
+              <Link
+                href={review.trailer}
+                target='_blank'
+                className='inline-block text-white w-full text-center mt-5 py-2 px-5 rounded-full border text-lg bg-purple-500 dark:bg-purple-400 hover:text-purple-400 hover:border-purple-400 hover:bg-transparent dark:hover:bg-transparent  font-medium'
+              >
+                Watch Trailer
+              </Link>
+            )}
+            <button className='inline-block text-white w-full text-center mt-5 py-2 px-5 rounded-full border text-lg bg-purple-500 dark:bg-purple-400 hover:text-purple-400 hover:border-purple-400 hover:bg-transparent dark:hover:bg-transparent  font-medium'>
+              Add to watchlist
+            </button>
           </div>
         </div>
         <div className='flex justify-between flex-wrap mt-5 '>
           <div className='w-full lg:w-[70%] xl:w-[77%]'>
-
-            <p className="mt-2  tracking-wider text-lg text-purple-500 dark:text-purple-400  font-medium">Review :</p>
-            <textarea className='bg-transparent w-full  resize-none h-screen text-justify mt-5 pr-2' value={review.detail} disabled>
-
-            </textarea>
-
+            <p className='mt-2  tracking-wider text-lg text-purple-500 dark:text-purple-400  font-medium'>
+              Review :
+            </p>
+            <textarea
+              className='bg-transparent w-full  resize-none h-screen text-justify mt-5 pr-2'
+              value={review.detail}
+              disabled
+            ></textarea>
           </div>
-          <div className="w-full lg:w-[25%] xl:w-[20%]">
-            <p className="mt-2  tracking-wider text-lg text-purple-500 dark:text-purple-400  font-medium">comments:</p>
+          <div className='w-full lg:w-[25%] xl:w-[20%]'>
+            <p className='mt-2  tracking-wider text-lg text-purple-500 dark:text-purple-400  font-medium'>
+              comments:
+            </p>
             <CommentForm reviewid={review._id} />
             <div>
-
-              {
-                (review.comments.length > 0) ? (
-                  <div className='mt-5 max-h-[50vh] lg:max-h-[90vh] overflow-y-auto'>
-
-                    {
-                      review.comments?.map((comment) => {
-
-                        return (
-                          <div key={comment.name} className=' mt-4 rounded-lg bg-slate-100 dark:bg-gray-700 p-3'>
-
-                            <div className='py-1 px-2 flex border-b dark:border-gray-500 justify-between'>
-                              <div className='flex items-center'>{comment.useravatar ? (<img src={comment.useravatar} alt={comment.createdby} className='border dark:border-slate-400 mr-1 w-7 h-7 rounded-full' />) : <AiOutlineUser size={20} className="border dark:border-slate-400 mr-1 w-7 h-7 rounded-full" />} {comment.username}</div>
-                              {
-                                session && ( (review.creator.userid == session.user?.id) || (comment._id == session.user?.id) || (session.user.role == "admin")) ? (<div><DelCommentBtn revid={review._id} commid={comment._id} />
-                                </div>) : null}
-
-                            </div>
-                            <div className='py-1 px-2'>{comment.comment}</div>
+              {review.comments.length > 0 ? (
+                <div className='mt-5 max-h-[50vh] lg:max-h-[90vh] overflow-y-auto'>
+                  {review.comments?.map(comment => {
+                    return (
+                      <div
+                        key={comment.name}
+                        className=' mt-4 rounded-lg bg-slate-100 dark:bg-gray-700 p-3'
+                      >
+                        <div className='py-1 px-2 flex border-b dark:border-gray-500 justify-between'>
+                          <div className='flex items-center'>
+                            {comment.useravatar ? (
+                              <img
+                                src={comment.useravatar}
+                                alt={comment.createdby}
+                                className='border dark:border-slate-400 mr-1 w-7 h-7 rounded-full'
+                              />
+                            ) : (
+                              <AiOutlineUser
+                                size={20}
+                                className='border dark:border-slate-400 mr-1 w-7 h-7 rounded-full'
+                              />
+                            )}{' '}
+                            {comment.username}
                           </div>
-
-                        );
-                      })
-                    }
-                  </div>
-
-                )
-                  : <p className='mt-5'>No comments avaliable become the first</p>
-              }
+                          {session &&
+                          (review.creator.userid == session.user?.id ||
+                            comment._id == session.user?.id ||
+                            session.user.role == 'admin') ? (
+                            <div>
+                              <DelCommentBtn
+                                revid={review._id}
+                                commid={comment._id}
+                              />
+                            </div>
+                          ) : null}
+                        </div>
+                        <div className='py-1 px-2'>{comment.comment}</div>
+                      </div>
+                    )
+                  })}
+                </div>
+              ) : (
+                <p className='mt-5'>No comments avaliable become the first</p>
+              )}
             </div>
           </div>
         </div>
-        <span className="mt-4 flex items-center text-base font-semibold  text-purple-700 dark:text-purple-400 ">
-          Review Posted by : {review.creator.avatar ? (<img src={review.creator.avatar} alt={review.creator.createdby} className='ml-3 mr-1 w-7 h-7 rounded-full' />) : <AiOutlineUser size={20} className="ml-3 mr-1 w-7 h-7 rounded-full" />} {review.creator.createdby}
+        <span className='mt-4 flex items-center text-base font-semibold  text-purple-700 dark:text-purple-400 '>
+          Review Posted by :{' '}
+          {review.creator.avatar ? (
+            <img
+              src={review.creator.avatar}
+              alt={review.creator.createdby}
+              className='ml-3 mr-1 w-7 h-7 rounded-full'
+            />
+          ) : (
+            <AiOutlineUser
+              size={20}
+              className='ml-3 mr-1 w-7 h-7 rounded-full'
+            />
+          )}{' '}
+          {review.creator.createdby}
         </span>
       </div>
     </section>
---
+++ app/user/watchlist/page.jsx
@@ -0,0 +1,13 @@
+const Watchlist = () => {
+  return (
+    <section className='px-2 mx-auto max-w-[1500px] md:pt-20 pt-16'>
+      <div className='container px-6 py-5 mx-auto'>
+        <h2 className='mb-6 text-[32px] font-bold capitalize text-dark lg:text-[4xl]'>
+          My Watchlist
+        </h2>
+      </div>
+    </section>
+  )
+}
+
+export default Watchlist
