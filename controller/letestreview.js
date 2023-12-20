bdf067f32dc3f70d32790e7007432fe6e7bbb1f9
Umang Sailor <ursailor@gmail.com>
2023-12-20 18:45:33 +0530

    fix: optimise home page

--- controller/letestreview.js
+++ controller/letestreview.js
@@ -1,14 +1,27 @@
-export default async function getLetestReview() {
-    try {
-      const api = process.env.API_URL;
-      const response = await fetch(`${api}/api/homepage?sort=-1`,      
-      {
-        cache: "no-store",
-      });
-      
-      const reviews = await response.json();
-      return reviews.data;
-    } catch (error) {
-      console.log(error);
+let cachedData = null;
+let lastFetchTime = 0;
+const refreshInterval = 2 * 60 * 1000; 
+
+export default async function getLatestReview() {
+  try {
+    const currentTime = new Date().getTime();
+
+    
+    if (cachedData && currentTime - lastFetchTime < refreshInterval) {
+      return cachedData;
     }
+
+    const api = process.env.API_URL;
+    const response = await fetch(`${api}/api/homepage?sort=-1`, {
+      cache: "no-store",
+    });
+
+    const reviews = await response.json();
+    cachedData = reviews.data;
+    lastFetchTime = currentTime;
+
+    return cachedData;
+  } catch (error) {
+    console.log(error);
   }
+}
--- controller/popularreview.js
+++ controller/popularreview.js
@@ -1,13 +1,27 @@
+let cachedData = null;
+let lastFetchTime = 0;
+const refreshInterval = 2 * 60 * 1000; 
+
 export default async function getPopularReview() {
-    try {
-      const api = process.env.API_URL;
-      const response = await fetch(`${api}/api/homepage/category?sort=1`,      
-      {
-        cache: "no-store",
-      });
-      const reviews = await response.json();
-      return reviews.data;
-    } catch (error) {
-      console.log(error);
+  try {
+    const currentTime = new Date().getTime();
+
+    
+    if (cachedData && currentTime - lastFetchTime < refreshInterval) {
+      return cachedData;
     }
+
+    const api = process.env.API_URL;
+    const response = await fetch(`${api}/api/homepage/category?sort=1`, {
+      cache: "no-store",
+    });
+
+    const reviews = await response.json();
+    cachedData = reviews.data;
+    lastFetchTime = currentTime;
+
+    return cachedData;
+  } catch (error) {
+    console.log(error);
   }
+}
