dd8208074fadc41f2cedde9b0418b62c4f14fad4
Umang Sailor <ursailor@gmail.com>
2023-12-20 20:32:08 +0530

    fix : execute admin from users

--- app/api/user/route.js
+++ app/api/user/route.js
@@ -41,16 +41,16 @@ export async function GET(req,res) {
         const page = req.nextUrl.searchParams.get('page') || 1;
         const pageSize = 15;
         const skip = (page - 1) * pageSize;
+        const usernameFilter = { username: { $ne: 'admin' } };
+        const users = await User.find(usernameFilter).select("_id username name email role").skip(skip).limit(parseInt(pageSize));
 
-        const emails = await User.find().select("_id username name email role").skip(skip).limit(parseInt(pageSize));
-
-        const totalDocuments = await User.countDocuments();
+        const totalDocuments = await User.countDocuments(usernameFilter);
 
         const hasNextPage = skip + pageSize < totalDocuments;
 
         return NextResponse.json(
             {
-                data: emails,
+                data: users,
                 meta: {
                     totalDocuments,
                     totalPages: Math.ceil(totalDocuments / pageSize),
