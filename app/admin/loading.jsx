cd344f3e5a627e5a4b28073e51e3aaa8e6895477
Umang Sailor <61745264+TuathaDeLugh@users.noreply.github.com>
2023-12-29 23:45:17 +0530

    Update loading.jsx

--- app/admin/loading.jsx
+++ app/admin/loading.jsx
@@ -1,8 +1,12 @@
-import Progress from '@/components/layout/Progress'
 import React from 'react'
 
 export default function Aloading() {
   return (
-   <Progress/>
+  <div className='flex space-x-2 justify-center items-center h-screen animate-pulse'>
+  	<span className='sr-only'>Loading...</span>
+   	<div className='h-8 w-8 dark:bg-purple-400 bg-purple-600  rounded-full animate-bounce  [animation-delay:-0.3s]'></div>
+ 	<div className='h-8 w-8 dark:bg-purple-400 bg-purple-600 rounded-full animate-bounce  [animation-delay:-0.15s]'></div>
+ 	<div className='h-8 w-8 dark:bg-purple-400 bg-purple-600 rounded-full animate-bounce '></div>
+ </div>
   )
 }
