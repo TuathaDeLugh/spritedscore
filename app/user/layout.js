62fe7306e083d5c15ee093902cacc9289355317d
Umang Sailor <ursailor@gmail.com>
2023-12-29 10:40:58 +0530

    refactor: remove usless import

--- app/user/layout.js
+++ app/user/layout.js
@@ -1,8 +1,6 @@
 import React from 'react'
 import { authOptions } from '../api/auth/[...nextauth]/route'
 import { getServerSession } from 'next-auth'
-import { redirect } from 'next/navigation'
-import { toast } from 'react-toastify'
 
 export  default async function layout({ children }) {
     const session = await getServerSession(authOptions)
