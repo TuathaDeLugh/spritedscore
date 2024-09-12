7df7f07ce82a9c90a031618db3ed7c834482aec6
Umang Sailor <ursailor@gmail.com>
2024-09-12 19:51:34 +0530

    sign in fix

--- yupschema/index.js
+++ yupschema/index.js
@@ -1,7 +1,7 @@
 import * as Yup from "yup";
 
 export const loginSchema =  Yup.object({
-    email: Yup.string().min(2).max(25).required("Please enter email"),
+    email: Yup.string().min(7).required("Please enter email"),
     password: Yup.string().min(3).required("Please enter password"),
   });
   export const UsernameSchema =  Yup.object({
